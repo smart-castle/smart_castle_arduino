7cedf5780ab00c60a31d82ce9974735bcd378354
Kollock <maxklein96@web.de>
2020-03-08 12:03:32 +0100

    added secrets

--- doorEntrySensor/doorEntrySensor.ino
+++ doorEntrySensor/doorEntrySensor.ino
@@ -1,9 +1,25 @@
-void setup() {
-  // put your setup code here, to run once:
+#define button1 2
+#define button2 3 
 
+void setup() {
+  //Inputs
+  pinMode(button1, INPUT_PULLUP);
+  pinMode(button2, INPUT_PULLUP);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  attachInterrupt(digitalPinToInterrupt(button1), easyGameInterupt1, FALLING);
+  attachInterrupt(digitalPinToInterrupt(button2), easyGameInterupt2, FALLING);
 
+  while(true){    
+  }
+
+}
+
+void easyGameInterupt1() {
+  Serial.print("Button 1 pressed \n");
+}
+
+void easyGameInterupt2() {
+  Serial.print("Button 2 pressed \n");
 }
---
+++ doorEntrySensor/secrets.ino
@@ -0,0 +1,4 @@
+#define FIREBASE_HOST
+#define FIREBASE_AUTH
+#define WIFI_SSID
+#define WIFI_PASSWORD
